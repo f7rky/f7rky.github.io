ffdd377702add9f199fe20827ff596e860628f47
itsmarian <100617987+itsmarianmc@users.noreply.github.com>
2026-02-02 18:55:41 +0100

    Added socials

---
+++ socials/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelectorAll('.fadeIn').forEach(el => {
+		el.addEventListener('animationend', () => {
+			el.classList.remove('fadeIn');
+		});
+	});
+});
+
+document.getElementById("linkid=gn3Wdalz").addEventListener("click", function() {
+	window.open("https://discord.com/users/1158514572609724518", "_blank")
+});
+
+document.getElementById("linkid=fdp329jm").addEventListener("click", function() {
+	window.open("https://discord.gg/hdMzYKkXxw", "_blank")
+});
+
+document.getElementById("linkid=sS7hJnkH").addEventListener("click", function() {
+	window.open("https://open.spotify.com/playlist/76DhwtRMaJYEItB6siGSfm", "_blank")
+});
+
+document.getElementById("linkid=QyoVosxc").addEventListener("click", function() {
+	window.open("https://www.youtube.com/@F7RKY08?sub_confirmation=1", "_blank")
+});
